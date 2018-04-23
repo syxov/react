82868b76ce971cd3612822af3db7d94cfaf0bc65
Aliaksandr Zasim <al.zasim@gmail.com>
2018-04-23 19:25:14 +0300

    Add jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,56 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
-module.exports = {
-    entry: {
-        'app': './src/main.jsx'
-    },
+module.exports = function (env, options) {
+    const isProduction = options.mode === 'production';
 
-    resolve: {
-        extensions: ['.jsx', '.js', '.less']
-    },
+    return {
+        devtool: isProduction ? 'source-map' : 'eval-source-map',
 
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
-    },
+        entry: {
+            'app': './src/main.jsx'
+        },
 
-    devServer: {
-        port: 9000,
-        contentBase: 'dist/'
-    },
+        resolve: {
+            extensions: ['.jsx', '.js', '.less']
+        },
 
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                loader: 'html-loader?caseSensitive=true'
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file-loader?name=assets/[name].[hash].[ext]'
-            },
-            {
-                test: /\.json$/,
-                loader: 'json-loader'
-            },
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-            }
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: 'bundle.js'
+        },
+
+        devServer: {
+            port: 9000,
+            contentBase: 'dist/'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader?caseSensitive=true'
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                    loader: 'file-loader?name=assets/[name].[hash].[ext]'
+                },
+                {
+                    test: /\.json$/,
+                    loader: 'json-loader'
+                },
+                {
+                    test: /\.jsx?$/,
+                    loader: 'babel-loader',
+                }
+            ]
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                inject: 'body'
+            })
         ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            inject: 'body'
-        })
-    ],
-
-    devtool: 'eval-source-map'
+    };
 };
