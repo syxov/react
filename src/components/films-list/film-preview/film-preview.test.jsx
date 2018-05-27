7b9218ee65f8f9987e65f15804d52dbfb1391a57
Aliaksandr Zasim <al.zasim@gmail.com>
2018-05-27 11:19:44 +0300

    Add a snapshot test

---
+++ src/components/films-list/film-preview/film-preview.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {FilmPreview} from './film-preview';
+
+test('FilmPreview should display film info', () => {
+    const filmInfo = {
+        filmName: 'Lorem Ipsulum',
+		filmCreationYear: 2000,
+		filmGenre: ['Lorem', 'Ipsum']
+    };
+
+	const component = renderer.create(
+		<FilmPreview filmName={filmInfo.filmName}  filmCreationYear={filmInfo.filmCreationYear}  filmGenre={filmInfo.filmGenre}/>,
+	);
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
